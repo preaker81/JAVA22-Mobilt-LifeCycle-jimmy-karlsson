3cf745a7a7bcc07fc53b73bc56aa5cf00f6830a7
Jimmy Karlsson <47915308+preaker81@users.noreply.github.com>
2023-09-08 19:14:36 +0200

    Cleaned up the code in DashboardActivity

--- app/src/main/java/com/example/lifecycle/DashboardActivity.kt
+++ app/src/main/java/com/example/lifecycle/DashboardActivity.kt
@@ -3,7 +3,6 @@ package com.example.lifecycle
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ArrayAdapter
@@ -18,84 +17,125 @@ import androidx.appcompat.widget.Toolbar
 
 class DashboardActivity : AppCompatActivity() {
 
-    lateinit var sharedPreferences: SharedPreferences
-    lateinit var dataFragment: DataFragment
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var dataFragment: DataFragment
 
-    val namePattern = "^[a-zA-Z ]{3,}$".toRegex()
-    val emailPattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$".toRegex()
-
+    // Regex patterns for validation
+    private val namePattern = "^[a-zA-Z ]{3,}$".toRegex()
+    private val emailPattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$".toRegex()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
+        // Initialize SharedPreferences and DataFragment
         sharedPreferences = getSharedPreferences("MyApp", MODE_PRIVATE)
-
-        // Setup the Toolbar
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
-
-        val logoutButtonDashboard: Button = findViewById(R.id.logoutButtonDashboard)
-
-        logoutButtonDashboard.setOnClickListener {
-            sharedPreferences.edit().putBoolean("isLoggedIn", false).apply()
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
-
-        val yearSpinner: Spinner = findViewById(R.id.yearSpinner)
-        val years = Array(124) { i -> (1900 + i).toString() }
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, years)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        yearSpinner.adapter = adapter
-
-        yearSpinner.setSelection(81)
-
-        val saveButton: Button = findViewById(R.id.saveButton)
-
-        // Initialize DataFragment
         dataFragment = DataFragment()
 
-        saveButton.setOnClickListener {
-            val fullName = findViewById<EditText>(R.id.fullNameEditText).text.toString()
-            val email = findViewById<EditText>(R.id.emailEditText).text.toString()
+        // Setup UI elements
+        setupToolbar()
+        setupLogoutButton()
+        setupYearSpinner()
+        setupSaveButton()
 
-            // Validate name
-            if (!isValidInput(fullName, namePattern)) {
-                findViewById<EditText>(R.id.fullNameEditText).error = "Invalid name"
-                return@setOnClickListener
-            }
-
-            // Validate email
-            if (!isValidInput(email, emailPattern)) {
-                findViewById<EditText>(R.id.emailEditText).error = "Invalid email"
-                return@setOnClickListener
-            }
-
-            // Your existing code for other fields
-            val yearOfBirth = findViewById<Spinner>(R.id.yearSpinner).selectedItem.toString()
-            val certified = findViewById<Switch>(R.id.certifiedSwitch).isChecked
-            val genderId = findViewById<RadioGroup>(R.id.genderRadioGroup).checkedRadioButtonId
-            val gender = findViewById<RadioButton>(genderId).text.toString()
-
-            // Saving data into SharedPreferences and updating DataFragment
-            val dataString =
-                "Full Name: $fullName\nEmail: $email\nYear of Birth: $yearOfBirth\nCertified: $certified\nGender: $gender"
-            sharedPreferences.edit().putString("userData", dataString).apply()
-
-            dataFragment.setData(dataString)
-        }
-
+        // Add DataFragment to UI
         supportFragmentManager.beginTransaction().add(R.id.fragmentContainer, dataFragment).commit()
     }
 
     override fun onResume() {
         super.onResume()
         val savedData = sharedPreferences.getString("userData", "")
-        if (savedData != "") {
-            dataFragment.setData(savedData!!)
+        savedData?.let {
+            if (it.isNotEmpty()) dataFragment.setData(it)
         }
+    }
+
+    // Setup the toolbar
+    private fun setupToolbar() {
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    // Setup the logout button
+    private fun setupLogoutButton() {
+        val logoutButtonDashboard: Button = findViewById(R.id.logoutButtonDashboard)
+        logoutButtonDashboard.setOnClickListener {
+            sharedPreferences.edit().putBoolean("isLoggedIn", false).apply()
+            navigateToMainActivity()
+        }
+    }
+
+    // Setup the year spinner
+    private fun setupYearSpinner() {
+        val yearSpinner: Spinner = findViewById(R.id.yearSpinner)
+        val years = Array(124) { i -> (1900 + i).toString() }
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, years)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        yearSpinner.adapter = adapter
+        yearSpinner.setSelection(81)
+    }
+
+    // Setup the save button and its click listener
+    private fun setupSaveButton() {
+        val saveButton: Button = findViewById(R.id.saveButton)
+        saveButton.setOnClickListener {
+            handleSaveUserData()
+        }
+    }
+
+    // Handles the saving of user data
+    private fun handleSaveUserData() {
+        val fullNameEditText = findViewById<EditText>(R.id.fullNameEditText)
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+        val fullName = fullNameEditText.text.toString()
+        val email = emailEditText.text.toString()
+
+        if (!isValidInput(fullName, namePattern)) {
+            fullNameEditText.error = "Invalid name"
+            return
+        }
+
+        if (!isValidInput(email, emailPattern)) {
+            emailEditText.error = "Invalid email"
+            return
+        }
+
+        val yearOfBirth = findViewById<Spinner>(R.id.yearSpinner).selectedItem.toString()
+        val certified = findViewById<Switch>(R.id.certifiedSwitch).isChecked
+        val genderId = findViewById<RadioGroup>(R.id.genderRadioGroup).checkedRadioButtonId
+        val gender = findViewById<RadioButton>(genderId).text.toString()
+
+        val dataString = buildDataString(fullName, email, yearOfBirth, certified, gender)
+        saveDataToPreferences(dataString)
+        dataFragment.setData(dataString)
+    }
+
+    // Build the data string
+    private fun buildDataString(
+        fullName: String,
+        email: String,
+        yearOfBirth: String,
+        certified: Boolean,
+        gender: String
+    ): String {
+        return "Full Name: $fullName\nEmail: $email\nYear of Birth: $yearOfBirth\nCertified: $certified\nGender: $gender"
+    }
+
+    // Save the data string to SharedPreferences
+    private fun saveDataToPreferences(dataString: String) {
+        sharedPreferences.edit().putString("userData", dataString).apply()
+    }
+
+    // Navigates to the MainActivity
+    private fun navigateToMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        startActivity(intent)
+    }
+
+    // Validates the given input string against the given pattern
+    private fun isValidInput(input: String, pattern: Regex): Boolean {
+        return pattern.matches(input)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -104,21 +144,13 @@ class DashboardActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        Log.d("DashboardActivity", "Menu item clicked: ${item.itemId}")
         return when (item.itemId) {
             R.id.action_main -> {
-                val intent = Intent(this, MainActivity::class.java)
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
-                startActivity(intent)
+                navigateToMainActivity()
                 true
             }
-
             else -> super.onOptionsItemSelected(item)
         }
-    }
-
-    fun isValidInput(input: String, pattern: Regex): Boolean {
-        return pattern.matches(input)
     }
 
 }
